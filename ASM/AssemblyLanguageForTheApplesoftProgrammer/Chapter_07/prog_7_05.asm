d9be6d92ad5600440d3e08a5fb70947d0862e4a0
Dave LeCompte <tsmaster@gmail.com>
2023-01-24 10:06:25 -0800

    Chap 7 Program 1, WIP
    looks like we're trying to do a branch to an absolute addr, that won't work.

---
+++ ASM/AssemblyLanguageForTheApplesoftProgrammer/Chapter_07/prog_7_05.asm
@@ -0,0 +1,43 @@
+;;; Chapter 7 Program 7.5
+;;; NOT YET WORKING
+;;;   - incorrect branch in line 1180
+;;;   - incorrect branch in line 1230
+;;;   - something weird around 1240
+;;;   - validate output from 0x316 on
+
+
+        .ORG $300
+        .OUT "prog_7_05.obj"
+
+;;; set up "&" vector on BRUN
+
+        LDA #$4C
+        STA $3F5
+        CLC
+        LDA $AA72
+        ADC #$17                ; $17 plus
+        STA $3f6                ; destination of
+        LDA $AA73               ; file most
+        ADC #$00                ; recently BLOADed
+        STA $3F7
+        RTS
+
+;;; ----------------------------------------
+;;; interpret "&" command string
+
+        LDY #$00
+a:      LDA ($B8),Y             ; read character
+        LDX #$08                ; test against 8 commands
+b:      DEX
+        BMI ret                 ; no match, quit
+        CMP data,X
+        BNE b                   ; not a match
+        STA $C050,X             ; toggle soft switch
+        INC $B8
+        BNE c                   ;   increment txtptr
+        INC $B9
+c:      BNE a
+ret:    RTS
+data:   .AS "GTFM12LH"
+        
+        
